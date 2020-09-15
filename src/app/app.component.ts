2b23dd93a9ec94050dce411c17495d9dbae521a0
Sandeep Kumar <sandeep.kumar@rootinfosolutions.net>
2020-09-15 18:55:55 +0530

    my-project

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  title = 'adds';
+  
+  ngOnInit(){  }
+}
